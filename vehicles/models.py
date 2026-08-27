782d20f9576bb25d2b2d4d17bc878ee44abd49f2
Yash Pratap Singh <yashpratapsingh@Yashs-MacBook-Air.local>
2026-08-27 20:57:36 +0530

    Making Vehicle model

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+     VEHICLE_TYPE_CHOICES = [
+        ("scooter", "Scooter"),
+        ("motorcycle", "Motorcycle"),
+    ]
+
+     TRANSMISSION_CHOICES = [
+        ("automatic", "Automatic"),
+        ("manual", "Manual"),
+    ]
+     name=models.CharField(max_length=100)
+     brand=models.CharField(max_length=100)
+     vehicle_type=models.CharField(max_length=20, choices=VEHICLE_TYPE_CHOICES)
+     transmission=models.CharField(max_length=20, choices=TRANSMISSION_CHOICES)
+     price_per_day = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+     seates=models.PositiveIntegerField(default=2)
+     is_available=models.BooleanField(default=True)
+     created_at=models.DateTimeField(auto_now_add=True)
+     def __str__(self):
+        return f"{self.brand} {self.name}"
